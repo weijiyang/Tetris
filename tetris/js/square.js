3505d641f9d2936a341f0b784472c11ac94a2e00
weijiyang <crawling_snail@163.com>
2018-09-29 16:49:52 +0800

    移动&旋转

---
+++ tetris/js/square.js
@@ -0,0 +1,92 @@
+var Square = function(){
+	//方块数据
+	this.data = [
+		[0, 2, 0],
+		[0, 2, 0],
+		[0, 2, 0]
+	];
+	//原点
+	this.origin = {
+		x : 0,
+		y : 0
+	}
+	//旋转角度
+	this.dir = 0;
+	//旋转数组
+	this.rotates = [
+		[
+			[0, 2, 0],
+			[0, 2, 0],
+			[0, 2, 0]
+		],
+		[
+			[0, 0, 0,],
+			[2, 2, 2,],
+			[0, 0, 0,]
+		],
+		[
+			[0, 2, 0],
+			[0, 2, 0],
+			[0, 2, 0]
+		],
+		[
+			[0, 0, 0,],
+			[2, 2, 2,],
+			[0, 0, 0,]
+		]
+	];
+
+
+}
+Square.prototype.canRotate = function(isValid){
+	var d = (this.dir + 1)%4;
+	var test = [
+		[0, 0, 0],
+		[0, 0, 0],
+		[0, 0, 0]
+	];
+	for(var i=0;i<this.data.length;i++){
+		for(var j=0;j<this.data[i].length;j++){
+			test[i][j] = this.rotates[d][i][j];
+		}
+	}
+	return isValid(this.origin,test)
+}
+Square.prototype.rotate = function(){
+	this.dir = (this.dir+1)%4;
+	for(var i=0; i<this.data.length;i++){
+		for(var j=0;j<this.data[i].length;j++){
+			this.data[i][j] = this.rotates[this.dir][i][j]
+		}
+	}
+}
+Square.prototype.canDown = function(isValid){
+	
+	var test = {};
+	test.x = this.origin.x +1 ;
+	test.y = this.origin.y;
+	return isValid(test,this.data)
+}
+Square.prototype.down = function(){
+	this.origin.x = this.origin.x + 1;
+}
+Square.prototype.canLeft = function(isValid){
+	
+	var test = {};
+	test.x = this.origin.x ;
+	test.y = this.origin.y - 1;
+	return isValid(test,this.data)
+}
+Square.prototype.left = function(){
+	this.origin.y = this.origin.y - 1;
+}
+Square.prototype.canRight = function(isValid){
+	
+	var test = {};
+	test.x = this.origin.x  ;
+	test.y = this.origin.y + 1;
+	return isValid(test,this.data)
+}
+Square.prototype.right = function(){
+	this.origin.y = this.origin.y + 1;
+}
